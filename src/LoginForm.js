2ef4b8cfc55c79c5678a765f2680c827966cb61e
Jüri Kajava <73351309+kajava-juri@users.noreply.github.com>
2022-08-31 14:31:29 +0300

    added register

--- src/LoginForm.js
+++ src/LoginForm.js
@@ -1,6 +1,8 @@
 import './LoginForm.css';
 import { useState } from 'react';
 import useSWR from 'swr';
+import { useEffect } from 'react';
+import { isDisabled } from '@testing-library/user-event/dist/utils';
 
 
 export function LoginForm({setUser, setToken}){
@@ -9,20 +11,33 @@ export function LoginForm({setUser, setToken}){
         username: "", 
         password: ""
     };
-    let [loginValues, setLoginValues] = useState(initialValues);
+    const registerInit = {
+        username: "",
+        firstname: "",
+        lastname: "",
+        newPassword: ""
+    }
 
-    function handleSignup(){
+    let [loginValues, setLoginValues] = useState(initialValues);
+    let [registerValues, setregisterValues] = useState(registerInit);
+
+    let [createIsDisabled, setcreateIsDisabled] = useState(true);
+
+    function handleSignup(e){
+        e.preventDefault();
 
         const Http = new XMLHttpRequest();
         const url='http://demo2.z-bit.ee/users';
         Http.open("POST", url);
-        Http.send();
+        Http.setRequestHeader("Content-Type", "application/json");
+        console.log(registerValues);
+        Http.send(JSON.stringify(registerValues));
         
-        Http.onreadystatechange = (e) => {
-          //console.log(Http.responseText)
-          console.log(Http.responseText["access_token"])
-          //setToken(Http.responseText["access_token"]);
-          //setUser(true)
+        Http.onreadystatechange = function(){
+            if(this.readyState === 4 && this.status===200){
+                console.log(Http.responseText);
+            }
+          
         }
     }
 
@@ -46,57 +61,67 @@ export function LoginForm({setUser, setToken}){
         }
     }
 
-    const handleInputChange = (e) => {
+    useEffect(() => {
+        let count = 0;
+        for (const [key, value] of Object.entries(registerValues)) {
+            //console.log(`${key}: ${value}`);
+            
+            if(value.length !== 0){
+                count++;
+                console.log(value.length);
+            }
+        }
+        if(count === 4){
+            setcreateIsDisabled(false);
+        }
+      }, [registerValues]);
+
+
+    const handleLoginInputChange = (e) => {
         //const name = e.target.name 
         //const value = e.target.value 
         const { name, value } = e.target;
-    
+        
+        
         setLoginValues({
-          ...loginValues,
-          [name]: value,
+            ...loginValues,
+            [name]: value,
+            });
+
+    };
+
+    const handleRegisterInputChange = (e) => {
+        //const name = e.target.name 
+        //const value = e.target.value 
+        const { name, value } = e.target;
+        
+        
+        setregisterValues({
+            ...registerValues,
+            [name]: value,
         });
-
     };
-
-    const login = {
-        username: "jyri.kajava@tptlive.ee", 
-        password: "duck123"
-    };
-
-
-    // const fetcher = (url) => fetch(url,
-    //     {
-    //         method: "POST",
-    //         headers: {"Content-Type": "application/json"},
-    //         body: JSON.stringify(login)
-    //     })
-    //     .then(res => res.json());
-    // var url = 'http://demo2.z-bit.ee/users/get-token';
-    // const { data, error } = useSWR(url, fetcher)
-
-    // if (error) console.log("failed to load");
-    // if (!data) console.log("loading...");
-    // console.log(data)
 
    return(
         <div>
             <h2>Create user</h2>
             <form onSubmit={handleSignup} id="signUp">
                 <label htmlFor="username">Username: </label>
-                <input type="text" name="username"/>
+                <input type="text" name="username" onChange={handleRegisterInputChange}/>
 
-                <label htmlFor="firstName">First name: </label>
-                <input type="text" name="firstName"/>
+                <label htmlFor="firstname">First name: </label>
+                <input type="text" name="firstname" onChange={handleRegisterInputChange}/>
 
-                <label htmlFor="username">Last name: </label>
-                <input type="text" name="username"/>
+                <label htmlFor="lastName">Last name: </label>
+                <input type="text" name="lastname" onChange={handleRegisterInputChange}/>
 
-                <label htmlFor="userPassword">Password: </label>
-                <input type="password" name="userPassword"/>
+                <label htmlFor="newPassword">Password: </label>
+                <input type="password" name="newPassword" onChange={handleRegisterInputChange}/>
 
 
                 {/* <button type="submit" onClick={() => setUser(true)}>Log in</button> */}
-                <input type="submit" value="Submit"></input>
+                <input type="submit" value="Submit" disabled={createIsDisabled}></input>
+                {createIsDisabled && (<p>Disabled</p>)}
                 
             </form>
 
@@ -104,10 +129,10 @@ export function LoginForm({setUser, setToken}){
             <form onSubmit={handlelogin}>
 
                 <label htmlFor="username">Username: </label>
-                <input type="text" name="username"  onChange={handleInputChange}/>
+                <input type="text" name="username"  onChange={handleLoginInputChange}/>
 
                 <label htmlFor="password">Password: </label>
-                <input type="password" name="password"  onChange={handleInputChange}/>
+                <input type="password" name="password"  onChange={handleLoginInputChange}/>
 
                 <input type="submit" value="Submit"></input>
                 
